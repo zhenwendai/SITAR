1233921cb4f888327346dc66a7963ddd0e67181c
Zhenwen Dai <z.dai@sheffield.ac.uk>
2017-03-15 20:41:22 +0000

    update the demo representation

--- sitar_model.py
+++ sitar_model.py
@@ -9,7 +9,7 @@ class SITAR(SSGPLVM):
     """
     Sparse latent varIable model of TrAnscritional Regulation (SITAR)
     """
-    def __init__(self, data, connM):
+    def __init__(self, data, connM,name='SITAR'):
         Y = data
 
         kern = GPy.kern.Linear(connM.shape[1],ARD=False,variances=1)
@@ -24,7 +24,7 @@ class SITAR(SSGPLVM):
 
 
         super(SITAR,self).__init__(Y, Z.shape[1],X=X_init, Z=Z, Gamma=X_prior, SLVM=True, alpha=2., beta=2., connM=connM.copy(),
-                       kernel=kern,num_inducing=connM.shape[1],group_spike=False)
+                       kernel=kern,num_inducing=connM.shape[1],group_spike=False,name=name)
 
         self.X.tau[:] = 2.
         self.X.gamma[connM==0].fix(warning=False)
@@ -34,6 +34,6 @@ class SITAR(SSGPLVM):
     def optimize(self,max_iters=1000,verbose=True,bfgs_factor=10,**kw):
 
         self.likelihood.variance.fix(warning=False)
-        super(SITAR,self).optimize(max_iters=max_iters/10,messages=verbose,bfgs_factor=bfgs_factor,**kw)
+        super(SITAR,self).optimize(max_iters=int(max_iters/10),messages=verbose,bfgs_factor=bfgs_factor,**kw)
         self.likelihood.variance.constrain_positive(warning=False)
         super(SITAR,self).optimize(max_iters=max_iters,messages=verbose,bfgs_factor=bfgs_factor,**kw)
